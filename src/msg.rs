91744bb90cddf967e911ab506c40bff62613d9c6
webelf101 <m63974529@gmail.com>
2025-02-28 05:06:43 +0800

    add missing features in release bobby function and add create bobby function

--- src/msg.rs
+++ src/msg.rs
@@ -1,5 +1,5 @@
 use cosmwasm_schema::{cw_serde, QueryResponses};
-use cosmwasm_std::{Binary, CustomMsg, Uint256};
+use cosmwasm_std::{Binary, CustomMsg, DenomUnit, Uint128, Uint256};
 
 #[allow(unused_imports)]
 use crate::state::State;
@@ -24,6 +24,10 @@ pub enum ExecuteMsg {
     ChangeAsset {
         new_c_asset: String,
         swap_info: SwapInfo,
+    },
+    CreateBobby {
+        compass_job_id: String,
+        blueprint: String,
     },
     SetBobby {
         bobby: String,
@@ -71,6 +75,53 @@ pub struct ExecuteJob {
 pub enum PalomaMsg {
     /// Message struct for cross-chain calls.
     SchedulerMsg { execute_job: ExecuteJob },
+
+    TokenFactoryMsg {
+        create_denom: Option<CreateDenomMsg>,
+        mint_tokens: Option<MintMsg>,
+    },
+
+    SkywayMsg {
+        set_erc20_to_denom: Option<SetErc20ToDenom>,
+        send_tx: Option<SendTx>,
+    },
+}
+
+#[cw_serde]
+pub struct CreateDenomMsg {
+    pub subdenom: String,
+    pub metadata: Metadata,
+}
+
+#[cw_serde]
+pub struct Metadata {
+    pub description: String,
+    pub denom_units: Vec<DenomUnit>,
+    pub base: String,
+    pub display: String,
+    pub name: String,
+    pub symbol: String,
+}
+
+#[cw_serde]
+pub struct MintMsg {
+    pub denom: String,
+    pub amount: Uint128,
+    pub mint_to_address: String,
+}
+
+#[cw_serde]
+pub struct SendTx {
+    pub remote_chain_destination_address: String,
+    pub amount: Uint128,
+    pub chain_reference_id: String,
+}
+
+#[cw_serde]
+pub struct SetErc20ToDenom {
+    pub erc20_address: String,
+    pub token_denom: String,
+    pub chain_reference_id: String,
 }
 
 impl CustomMsg for PalomaMsg {}
